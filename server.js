95155a860099ee78d011ca46309c74a7d387c984
simon.blanco <sr.pepinogalactico@gmail.com>
2022-07-20 00:15:23 -0300

    remove index.html (test file)

--- server.js
+++ server.js
@@ -1,63 +1,61 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import logger from "./src/shared/infrastructure/logger.js"
+import logger from "./src/shared/infrastructure/logger.js";
 import { registerRoutes } from "./src/shared/infrastructure/routes/index.js";
 import http from "http";
 
-import { emitter, registerEventListener } from "./src/shared/infrastructure/eventEmitter/index.js";
+import {
+  emitter,
+  registerEventListener,
+} from "./src/shared/infrastructure/eventEmitter/index.js";
 
 import createIo from "./src/shared/infrastructure/socketio.js";
 import { registerWebscoketEvents } from "./src/shared/infrastructure/webscoketEvents/index.js";
 
-import path from "path";
-
 export function Server(port) {
-    let httpServer;
-    const app = express();
-    app.use(cookieParser())
-    app.use(express.urlencoded({ extended: false, limit: "100mb" }));
-    app.use(express.json({ limit: "100mb" }));
-    app.use(cors({
-        origin: ['http://localhost:8080', "http://localhost:4000"],
-        credentials: true,
-    }));
-
-    const router = express.Router();
-    app.use(router)
-
-    const server = http.createServer(app);
-    const io = createIo(server);
-
-    registerRoutes(router, app);
-    registerEventListener(emitter, io);
-
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(process.cwd(), 'index.html'));
+  let httpServer;
+  const app = express();
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false, limit: "100mb" }));
+  app.use(express.json({ limit: "100mb" }));
+  app.use(
+    cors({
+      origin: ["http://localhost:8080", "http://localhost:4000"],
+      credentials: true,
     })
+  );
 
-    return {
-        listen: () => {
-            return new Promise(resolve => {
-                httpServer = server.listen(port, () => {
-                    io.on('connection', socket => {
-                        registerWebscoketEvents(socket);
-                    })
-                    logger.info(`Server listening on port ${port}`);
-                    resolve();
-                });
-            })
-        },
-        stop: () => {
-            return new Promise((resolve, reject) => {
-                if (!httpServer) return resolve()
-                httpServer.close(err => {
-                    if (err) reject(err);
-                    logger.info(`Server stopped`);
-                    resolve();
-                })
-            })
-        }
-    }
+  const router = express.Router();
+  app.use(router);
+
+  const server = http.createServer(app);
+  const io = createIo(server);
+
+  registerRoutes(router, app);
+  registerEventListener(emitter, io);
+
+  return {
+    listen: () => {
+      return new Promise((resolve) => {
+        httpServer = server.listen(port, () => {
+          io.on("connection", (socket) => {
+            registerWebscoketEvents(socket);
+          });
+          logger.info(`Server listening on port ${port}`);
+          resolve();
+        });
+      });
+    },
+    stop: () => {
+      return new Promise((resolve, reject) => {
+        if (!httpServer) return resolve();
+        httpServer.close((err) => {
+          if (err) reject(err);
+          logger.info(`Server stopped`);
+          resolve();
+        });
+      });
+    },
+  };
 }
-
--- src/product/infrastructure/product.momodel.js
+++ src/product/infrastructure/product.momodel.js
@@ -1,45 +1,43 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ProductSchema = Schema({
-    // _id: { type: mongoose.Types.ObjectId, required: false },
-    name: { type: String, required: false },
-    brand: {
-        _id: { type: Schema.Types.ObjectId, ref: 'Brand' },
-        name: String
-    },
-    category: {
-        name: String,
-        idsTree: [String],
-        breadcrumb: String,
-        _id: { type: mongoose.Types.ObjectId, ref: 'Category' },
-    },
-    images: { type: [String], required: false },
-    mainImage: { type: String, required: false },
-    stock: { type: Number, required: false },
-    description: { type: String, required: false },
-    price: {
-        cost: { type: Number, default: 0 },
-        sale: { type: Number, required: true },
-        taxs: [{ name: String, amount: Number }],
-        percentOfProfit: { type: Number, default: 0 },
-    },
-    wholesalerData: {
-        wholesalerId: { type: Schema.Types.ObjectId, ref: 'Wholesaler' },
-        name: { type: String, default: 'Cucumber' },
-        productPageUrl: String,
-        productCode: { type: String, unique: true },
-        images: [String],
-        description: String,
-    },
-    created: { type: Date, default: Date.now() },
-    customFeatures: [String],
-    categoryFeatures: [String],
-    moreInfoUrl: String,
-    ytVideos: [String],
-
+  // _id: { type: mongoose.Types.ObjectId, required: false },
+  name: { type: String, required: false },
+  brand: {
+    _id: { type: Schema.Types.ObjectId, ref: "Brand" },
+    name: String,
+  },
+  category: {
+    name: String,
+    idsTree: [String],
+    breadcrumb: String,
+    _id: { type: mongoose.Types.ObjectId, ref: "Category" },
+  },
+  images: { type: [String], required: false },
+  mainImage: { type: String, required: false },
+  stock: { type: Number, required: false },
+  description: { type: String, required: false },
+  price: {
+    cost: { type: Number, default: 0 },
+    sale: { type: Number, required: true },
+    taxs: [{ name: String, amount: Number }],
+    percentOfProfit: { type: Number, default: 0 },
+  },
+  wholesalerData: {
+    wholesalerId: { type: Schema.Types.ObjectId, ref: "Wholesaler" },
+    name: { type: String, default: "Cucumber" },
+    productPageUrl: String,
+    productCode: { type: String, unique: true },
+    images: [String],
+    description: String,
+  },
+  created: { type: Date, default: Date.now() },
+  customFeatures: [{}],
+  categoryFeatures: [{}],
+  moreInfoUrl: String,
+  ytVideos: [String],
 });
-export const ProductModel = mongoose.model('Product', ProductSchema);
-
+export const ProductModel = mongoose.model("Product", ProductSchema);
 
 /* 
 
